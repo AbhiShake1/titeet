b8dcfd1217d8cfc200c921e6753063c596f13b6c
abhishake1 <abhi9848038283@gmail.com>
2023-11-11 19:55:47 +0000

    refactor: car-table

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { Card } from "@car/ui";
 
 import { CreatePost } from "~/app/_components/create-post";
 import { api } from "~/trpc/server";
--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -3,8 +3,10 @@ import { type Config } from "drizzle-kit";
 export default {
   schema: "./src/schema.ts",
   driver: "mysql2",
+	verbose: false,
+	strict: false,
   dbCredentials: {
     connectionString: process.env.DATABASE_URL ?? "",
   },
-  tablesFilter: ["web_*"],
+  tablesFilter: ["car_*"],
 } satisfies Config;
