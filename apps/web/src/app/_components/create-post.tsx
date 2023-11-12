510f4a014f94bceed9da102cc42158d5c787a5a1
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2023-11-12 09:57:42 +0000

    feat: add car db schema (#4)

--- apps/web/src/app/_components/create-post.tsx
+++ apps/web/src/app/_components/create-post.tsx
@@ -6,12 +6,12 @@ import { clientApi } from "~/trpc/react";
 
 export function CreatePost() {
   const router = useRouter();
-  const [name, setName] = useState("");
+  const [model, setModel] = useState("");
 
   const createPost = clientApi.post.create.useMutation({
     onSuccess: () => {
       router.refresh();
-      setName("");
+      setModel("");
     },
   });
 
@@ -19,15 +19,23 @@ export function CreatePost() {
     <form
       onSubmit={(e) => {
         e.preventDefault();
-        createPost.mutate({ name });
+				createPost.mutate({
+          model,
+          year: 2000,
+          color: "red",
+          price: 40000000,
+					mileage: 12,
+					imageUrl: "",
+					manufacturer: "tesla",
+        });
       }}
       className="flex flex-col gap-2"
     >
       <input
         type="text"
         placeholder="Title"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
+        value={model}
+        onChange={(e) => setModel(e.target.value)}
         className="w-full rounded-full px-4 py-2 text-black"
       />
       <button
